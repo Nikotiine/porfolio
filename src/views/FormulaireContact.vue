5bd99427b2fc7f30548cb365ccdb6ac05f1a933e
Nikotiine <niko.tiine@gmail.com>
2022-06-22 17:47:46 +0200

    add recaptcha

--- src/views/FormulaireContact.vue
+++ src/views/FormulaireContact.vue
@@ -1,122 +1,124 @@
 <template>
   <section>
-    <div class="box">
-      <div class="field">
-        <label class="label">Votre Nom</label>
-        <div class="control has-icons-left has-icons-right">
-          <input
-            class="input"
-            type="text"
-            placeholder="Indiquez votre nom"
-            v-model="lastName"
-            :class="{ 'is-valid-input': this.lastName }"
-          />
-          <span
-            class="icon is-small is-left"
-            :class="{ 'has-text-info': this.lastName }"
-          >
-            <i class="fas fa-user"></i>
-          </span>
-          <span
-            class="icon is-small is-right has-text-success"
-            v-if="this.lastName"
-          >
-            <i class="fas fa-check"></i>
-          </span>
+    <form action="POST" id="form">
+      <div class="box">
+        <div class="field">
+          <label class="label">Votre Nom</label>
+          <div class="control has-icons-left has-icons-right">
+            <input
+              class="input"
+              type="text"
+              placeholder="Indiquez votre nom"
+              v-model="lastName"
+              :class="{ 'is-valid-input': this.lastName }"
+            />
+            <span
+              class="icon is-small is-left"
+              :class="{ 'has-text-info': this.lastName }"
+            >
+              <i class="fas fa-user"></i>
+            </span>
+            <span
+              class="icon is-small is-right has-text-success"
+              v-if="this.lastName"
+            >
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
         </div>
-      </div>
 
-      <div class="field">
-        <label class="label">Votre Prenom</label>
-        <div class="control has-icons-left has-icons-right">
-          <input
-            class="input"
-            type="text"
-            placeholder="Indiquez votre prenom"
-            :class="{ 'is-valid-input': this.firstName }"
-            v-model="firstName"
-          />
-          <span
-            class="icon is-small is-left"
-            :class="{ 'has-text-info': this.firstName }"
-          >
-            <i class="fas fa-user"></i>
-          </span>
-          <span
-            class="icon is-small is-right has-text-success"
-            v-if="this.firstName"
-          >
-            <i class="fas fa-check"></i>
-          </span>
+        <div class="field">
+          <label class="label">Votre Prenom</label>
+          <div class="control has-icons-left has-icons-right">
+            <input
+              class="input"
+              type="text"
+              placeholder="Indiquez votre prenom"
+              :class="{ 'is-valid-input': this.firstName }"
+              v-model="firstName"
+            />
+            <span
+              class="icon is-small is-left"
+              :class="{ 'has-text-info': this.firstName }"
+            >
+              <i class="fas fa-user"></i>
+            </span>
+            <span
+              class="icon is-small is-right has-text-success"
+              v-if="this.firstName"
+            >
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
         </div>
-      </div>
 
-      <div class="field">
-        <label class="label">Votre email</label>
-        <div class="control has-icons-left has-icons-right">
-          <input
-            class="input"
-            type="email"
-            placeholder="Indiquez votre Email"
-            v-model="email"
-            :class="{
-              'is-valid-input': validEmail,
-              'is-invalid-input': notValidEmail,
-            }"
-            @blur="testValidEmail"
-          />
-          <span
-            class="icon is-small is-left"
-            :class="{ 'has-text-info': validEmail }"
-          >
-            <i class="fas fa-envelope"></i>
-          </span>
-          <span
-            class="icon is-small is-right has-text-success"
-            v-if="validEmail"
-          >
-            <i class="fas fa-check"></i>
-          </span>
+        <div class="field">
+          <label class="label">Votre email</label>
+          <div class="control has-icons-left has-icons-right">
+            <input
+              class="input"
+              type="email"
+              placeholder="Indiquez votre Email"
+              v-model="email"
+              :class="{
+                'is-valid-input': validEmail,
+                'is-invalid-input': notValidEmail,
+              }"
+              @blur="testValidEmail"
+            />
+            <span
+              class="icon is-small is-left"
+              :class="{ 'has-text-info': validEmail }"
+            >
+              <i class="fas fa-envelope"></i>
+            </span>
+            <span
+              class="icon is-small is-right has-text-success"
+              v-if="validEmail"
+            >
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
         </div>
-      </div>
 
-      <div class="field">
-        <label class="label">Votre message</label>
-        <div class="control">
-          <textarea
-            :class="{ 'is-valid-input': this.message }"
-            class="textarea"
-            placeholder="Message"
-            v-model="message"
-          ></textarea>
+        <div class="field">
+          <label class="label">Votre message</label>
+          <div class="control">
+            <textarea
+              :class="{ 'is-valid-input': this.message }"
+              class="textarea"
+              placeholder="Message"
+              v-model="message"
+            ></textarea>
+          </div>
         </div>
-      </div>
 
-      <div class="field">
-        <div class="control">
-          <label class="checkbox">
-            <input type="checkbox" v-model="cgu" />
-            Je suis d'accord avec
-            <router-link to="/mentions-legales">
-              <strong>les CGU</strong>
-            </router-link>
-          </label>
+        <div class="field">
+          <div class="control">
+            <label class="checkbox">
+              <input type="checkbox" v-model="cgu" />
+              Je suis d'accord avec
+              <router-link to="/mentions-legales">
+                <strong>les CGU</strong>
+              </router-link>
+            </label>
+          </div>
         </div>
-      </div>
 
-      <div class="field is-grouped is-justify-content-center">
-        <div class="control">
-          <button
-            class="button is-primary"
-            :class="{ 'is-loading': waitForResult }"
-            @click="send"
-            :disabled="!fieldIsValid"
-          >
-            Envoyer
-          </button>
+        <div class="field is-grouped is-justify-content-center">
+          <div class="control">
+            <button
+              class="button is-primary"
+              :class="{ 'is-loading': waitForResult }"
+              @click="runCaptcha"
+              :disabled="!fieldIsValid"
+            >
+              Envoyer
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
     <toast-succes :active="succes" :message="succesMessage" />
     <toast-error :active="erreur" :message="errorMessage" />
   </section>
@@ -138,7 +140,7 @@ export default {
       firstName: null,
       email: null,
       cgu: false,
-
+      captchaKey: "6LcTQI4gAAAAAK-pOUH1f-gqPubfPz3FUf_MzNTJ",
       succes: false,
       erreur: false,
       waitForResult: false,
@@ -149,6 +151,18 @@ export default {
     };
   },
   methods: {
+    runCaptcha() {
+      /* eslint-disable */
+      grecaptcha.ready(function () {
+        grecaptcha
+          .execute(this.captchaKey, { action: "submit" })
+          .then(function (token) {
+            const captcha = token;
+            this.send(captcha);
+          });
+      });
+    },
+
     testValidEmail: function () {
       if (!this.testEmail) {
         this.notValidEmail = true;
@@ -161,7 +175,7 @@ export default {
     scrollUp() {
       window.scrollTo(0, 0);
     },
-    send: function () {
+    send: function (token) {
       this.waitForResult = true;
 
       axios
@@ -170,6 +184,7 @@ export default {
           lastName: this.lastName,
           email: this.email,
           message: this.message,
+          captchaToken: token,
         })
         .then((res) => {
           this.waitForResult = false;
@@ -207,7 +222,14 @@ export default {
       } else return true;
     },
   },
-  mounted() {},
+  mounted() {
+    let recaptchaScript = document.createElement("script");
+    recaptchaScript.setAttribute(
+      "src",
+      "https://www.google.com/recaptcha/api.js?render=6LcTQI4gAAAAAK-pOUH1f-gqPubfPz3FUf_MzNTJ"
+    );
+    document.head.appendChild(recaptchaScript);
+  },
 };
 </script>
 
